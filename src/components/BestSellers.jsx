d6604cae95006c490007aca7337e0711adf14b07
olearybmps <160647100+olearybmps@users.noreply.github.com>
2024-05-18 15:31:22 -0400

    Best sellers are by list and in rows/cards

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -1,29 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import './BestSellers.css';
 
 function BestSellers() {
-    const [books, setBooks] = useState([]);
+    const [lists, setLists] = useState([]);
 
     useEffect(() => {
-        const fetchBooks = async () => {
+        const fetchLists = async () => {
             const nytApiKey = import.meta.env.VITE_NYTIMES_API_KEY;
             const response = await fetch(
-                `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${nytApiKey}`
+                `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${nytApiKey}`
             );
             const data = await response.json();
-            setBooks(data.results?.books || []);
+            setLists(data.results.lists);
         };
 
-        fetchBooks();
+        fetchLists();
     }, []);
 
     return (
-        <div>
+        <div className="best-sellers">
             <h1>NYT Best Sellers</h1>
-            <ul>
-                {books.map((book) => (
-                    <li key={book.primary_isbn10}>{book.title}</li>
-                ))}
-            </ul>
+            {lists.map((list) => (
+                <div key={list.list_id} className="list">
+                    <h2>{list.display_name}</h2>
+                    <div className="book-grid">
+                        {list.books.map((book) => (
+                            <div
+                                key={book.primary_isbn10}
+                                className="book-card"
+                            >
+                                <img src={book.book_image} alt={book.title} />
+                                <div className="book-details">
+                                    <p>List Ranking: {book.rank}</p>
+                                    <h3>{book.title}</h3>
+                                    <p>{book.description}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
